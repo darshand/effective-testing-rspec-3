0d34484dbdd75294f27a77b2d4ae77e7e7ef45f7
darshand <darshan.bd@onze.in>
2018-07-11 14:46:12 +0530

    second example

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe 'An ideal sandwich' do
     taste = sandwich.taste
     expect(taste).to eq('delecious')
   end
+
+  it 'lets me add toppings' do
+    sandwich = Sandwich.new('delecious', [])
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
+
+    expect(toppings).not_to be_empty
+  end
 end
